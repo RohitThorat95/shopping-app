df218d4009bd902c8b6ad5e9d192e041a5c0723d
RohitThorat95 <thorat.rohit95@gmail.com>
2017-12-16 12:40:25 +0530

    first

--- seed/ProductSeeder.js
+++ seed/ProductSeeder.js
@@ -8,37 +8,37 @@ var products =  [
   new Product({
   imagePath: 'https://cdn.shopify.com/s/files/1/1027/1179/products/1_0b1756d7-91ff-4678-b55f-1d6fb6b41a81_grande.jpg?v=1445527685',
   title: 'Denim Jacket',
-  description: 'Jack n Jones Biker Denim Jacket',
+  description: 'only 2 in stock',
   price: 10
   }),
   new Product({
-  imagePath: 'https://cdn.shopify.com/s/files/1/1027/1179/products/1_0b1756d7-91ff-4678-b55f-1d6fb6b41a81_grande.jpg?v=1445527685',
-  title: 'Denim Jacket',
-  description: 'Jack n Jones Biker Denim Jacket',
+  imagePath: 'http://scene7.zumiez.com/is/image/zumiez/pdp_hero/LRG-Hustle-Trees-Red-Crew-Neck-Sweatshirt-_202001-0012-front.jpg',
+  title: 'Hustle Crew Neck',
+  description: '306 in stock',
   price: 20
   }),
   new Product({
-  imagePath: 'https://cdn.shopify.com/s/files/1/1027/1179/products/1_0b1756d7-91ff-4678-b55f-1d6fb6b41a81_grande.jpg?v=1445527685',
-  title: 'Denim Jacket',
-  description: 'Jack n Jones Biker Denim Jacket',
+  imagePath: 'https://cdn.shopify.com/s/files/1/0058/9552/products/793579761242_5.jpg?v=1508883866',
+  title: 'Get Shi*t Done Notebook',
+  description: '1006 in stock',
   price: 30
   }),
   new Product({
-  imagePath: 'https://cdn.shopify.com/s/files/1/1027/1179/products/1_0b1756d7-91ff-4678-b55f-1d6fb6b41a81_grande.jpg?v=1445527685',
-  title: 'Denim Jacket',
-  description: 'Jack n Jones Biker Denim Jacket',
+  imagePath: 'https://noveltystreet.com/wp-content/uploads/2015/09/Stone-Cask-Shot-Flask-Sanitary-Way-to-Share-Drink.jpg',
+  title: 'Take A Shot Flask',
+  description: '109 in stock',
   price: 40
   }),
   new Product({
-  imagePath: 'https://cdn.shopify.com/s/files/1/1027/1179/products/1_0b1756d7-91ff-4678-b55f-1d6fb6b41a81_grande.jpg?v=1445527685',
-  title: 'Denim Jacket',
-  description: 'Jack n Jones Biker Denim Jacket',
+  imagePath: 'http://cdn.shopify.com/s/files/1/0880/7334/products/Sweatshirt_2_grande.jpg?v=1477602322',
+  title: 'Shopify Sweatshirt',
+  description: '99 in stock',
   price: 50
   }),
   new Product({
-  imagePath: 'https://cdn.shopify.com/s/files/1/1027/1179/products/1_0b1756d7-91ff-4678-b55f-1d6fb6b41a81_grande.jpg?v=1445527685',
-  title: 'Denim Jacket',
-  description: 'Jack n Jones Biker Denim Jacket',
+  imagePath: 'https://cdn.shopify.com/s/files/1/0880/7334/products/OutboundCollective__34648_medium.jpg?v=1474484951',
+  title: 'shopify hat',
+  description: '199 in stock',
   price: 60
   })
  ];
